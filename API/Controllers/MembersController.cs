7bd0fc85045b36e7ddb75aa2fa07bf2fb23897f6
seif-aboelsaad <seif.aboelsaad10@gmail.com>
2025-08-16 01:47:37 +0300

    Sec 2

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        // GET: api/members
+        [HttpGet]
+        public async Task<IActionResult> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+            return Ok(members);
+        }
+        // GET: api/members/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMemberById(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return Ok(member);
+        }
+
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<API.Data.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
